47cc9984e274c010f6612751e0e7b170815c4dfa
Vinicius Martins <martinsrvinicius@gmail.com>
2024-02-12 20:02:59 +0000

    Routes redirecting and nesting

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -3,25 +3,26 @@ import Admin from "pages/Admin";
 import Catalog from "pages/Catalog";
 import Home from "pages/Home";
 import ProductDetails from "pages/ProductDetails";
-import { BrowserRouter, Switch, Route  } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 const Routes = () => (
     <BrowserRouter>
         <Navbar />
-            <Switch>
-                <Route path="/" exact>
-                    <Home />
-                </Route>
-                <Route path="/products" exact>
-                    <Catalog />
-                </Route>
-                <Route path="/products/:productId">
-                    <ProductDetails />
-                </Route>
-                <Route path="/admin">
-                    <Admin />
-                </Route>
-            </Switch>
+        <Switch>
+            <Route path="/" exact>
+                <Home />
+            </Route>
+            <Route path="/products" exact>
+                <Catalog />
+            </Route>
+            <Route path="/products/:productId">
+                <ProductDetails />
+            </Route>
+            <Redirect from="/admin" to="/admin/products" exact />
+            <Route path="/admin">
+                <Admin />
+            </Route>
+        </Switch>
     </BrowserRouter>
 );
 
